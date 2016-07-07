d17bd71d896505c5b7d2e70b9bc50c43beca80db
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-07 13:32:54 +0900

    [Fix] Number View Controller

--- SampleRxSwift/ViewController.swift
+++ SampleRxSwift/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var firstTextField: UITextField!
+    
+    @IBOutlet var secondTextField: UITextField!
+    
+    @IBOutlet var thirdTextField: UITextField!
+    
+    @IBOutlet var resultLabel: UILabel!
+    
+    var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Observable.combineLatest(firstTextField.rx_text, secondTextField.rx_text, thirdTextField.rx_text) { (textValue1, textValue2, textValue3) -> Int in
+            return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0)
+        }
+        .map { $0.description }
+        .bindTo(resultLabel.rx_text).addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
