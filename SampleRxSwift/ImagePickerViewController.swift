af6950517fc67cca56370ff0402fd42798385ca0
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-09 01:31:52 +0900

    [Add] camraButton rx tap

--- SampleRxSwift/ImagePickerViewController.swift
+++ SampleRxSwift/ImagePickerViewController.swift
@@ -7,13 +7,32 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ImagePickerViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var galleryButton: UIButton!
+    @IBOutlet var cropButton: UIButton!
+    
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+
+        cameraButton.rx_tap.flatMapLatest { [weak self] _ in
+            return UIImagePickerController.rx_createWithParent(self) { (picker) in
+                picker.sourceType = .Camera
+                picker.allowsEditing = false
+                }.flatMap({ $0.rx_didFinishPickingMediaWithInfo
+                }).take(1)
+        }.map { (info) in
+            return info[UIImagePickerControllerOriginalImage] as? UIImage
+        }.bindTo(imageView.rx_image).addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
