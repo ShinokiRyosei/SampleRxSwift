485bc2bcfc28e19b4704effed632333e4365e2fa
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-08 19:21:10 +0900

    [Create] ImagePickerViewController

---
+++ SampleRxSwift/ImagePickerViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePickerViewController.swift
+//  SampleRxSwift
+//
+//  Created by ShinokiRyosei on 2016/07/08.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+class ImagePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
