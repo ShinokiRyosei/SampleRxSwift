885fa650e243a559b4cbdc5ea22f3c81ca66ef78
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-09 18:53:15 +0900

    [Update] ImagePickerController rx method

--- SampleRxSwift/ImagePickerViewController.swift
+++ SampleRxSwift/ImagePickerViewController.swift
@@ -33,6 +33,26 @@ class ImagePickerViewController: UIViewController {
         }.map { (info) in
             return info[UIImagePickerControllerOriginalImage] as? UIImage
         }.bindTo(imageView.rx_image).addDisposableTo(disposeBag)
+        
+        galleryButton.rx_tap.flatMapLatest { [weak self] _ in
+            return UIImagePickerController.rx_createWithParent(self) { picker in
+                picker.sourceType = .PhotoLibrary
+                picker.allowsEditing = false
+            }.flatMap({
+                $0.rx_didFinishPickingMediaWithInfo
+            })}.take(1)
+            .map({ info in
+                return info[UIImagePickerControllerOriginalImage] as? UIImage
+            }).bindTo(imageView.rx_image).addDisposableTo(disposeBag)
+        
+        cropButton.rx_tap.flatMap { [weak self] _ in
+            return UIImagePickerController.rx_createWithParent(self) { picker in
+                picker.sourceType = .PhotoLibrary
+                picker.allowsEditing = true
+                }.flatMap({ $0.rx_didFinishPickingMediaWithInfo}).take(1)
+            }.map({ info in
+                return info[UIImagePickerControllerOriginalImage] as? UIImage
+            }).bindTo(imageView.rx_image).addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
