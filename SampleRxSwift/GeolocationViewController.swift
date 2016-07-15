d8ef279dedeaf7e638dde2dffabdb66ae4ce3426
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-15 16:14:36 +0900

    [Create] GeolocationViewController

---
+++ SampleRxSwift/GeolocationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GeolocationViewController.swift
+//  SampleRxSwift
+//
+//  Created by ShinokiRyosei on 2016/07/15.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+import CoreLocation
+
+class GeolocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
