d014801798264fa61c5d4bf763540d88ba876254
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-07-15 16:23:05 +0900

    [Add] private extension UILabel and UIView

--- SampleRxSwift/GeolocationViewController.swift
+++ SampleRxSwift/GeolocationViewController.swift
@@ -11,6 +11,28 @@ import RxCocoa
 import RxSwift
 import CoreLocation
 
+private extension UILabel {
+    var rx_driveCoordinates: AnyObserver<CLLocationCoordinate2D> {
+        return UIBindingObserver(UIElement: self) { label, location in
+            label.text = "Lat: \(location.latitude)\n Lon: \(location.longtitude)"
+        }.asObserver()
+    }
+}
+
+private extension UIView {
+    var rx_driveAuthorization: AnyObserver<Bool> {
+        return UIBindingObserver(UIElement: self) { view , authorized in
+            if authorized  {
+                view.hidden = true
+                view.subview?.sendSubViewToBack(view)
+            }else {
+                view.hidden = false
+                view.subview?.bringSubviewToFront(view)
+            }
+        }.asObserver()
+    }
+}
+
 class GeolocationViewController: UIViewController {
 
     override func viewDidLoad() {
